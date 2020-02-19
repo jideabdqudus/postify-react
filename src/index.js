6232eb82734f81b58c809a754a26e84b978bcd23
jideabdqudus <jideabdqudus@gmail.com>
2020-02-19 17:09:22 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Header =(props)=>{
+    return(
+     <h1>Postify</h1>
+    ) 
+ }
+
+class Postify extends React.Component{
+
+    constructor(props){ 
+        super(props);
+        this.state={
+            name: '',
+            email: ''
+        }
+        this.handleFormSubmitted=this.handleFormSubmitted.bind(this);
+        this.onChange = this.onChange.bind(this);
+    }
+
+    handleFormSubmitted=(event)=>{
+        event.preventDefault();
+        console.log(event.target.value);
+        this.setState({
+            posts:event.value
+        })
+    }
+    
+    onChange = (event) => {
+        this.setState((_prev) => {
+            _prev.name = event.target.value;
+        });
+    }
+
+    render(){
+        return( 
+            <div>
+                <h1>{this.state.name}</h1>
+                <form onSubmit={this.handleFormSubmitted}>
+                    <input name="name" onChange={() => this.onChange}/>
+                    <button onClick={this.onInput}>Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+
+ReactDOM.render(<Postify/>, document.getElementById('root'));
