9cabc204034718373827d318cecfbe3496245164
jideabdqudus <jideabdqudus@gmail.com>
2020-02-24 13:31:29 +0100

    changes to the components

--- src/index.js
+++ src/index.js
@@ -1,23 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+
 class App extends React.Component{
     constructor(props){
         super (props);
         this.addPost=this.addPost.bind(this);
+        this.handleDeleteAll=this.handleDeleteAll.bind(this);
         this.handlePost=this.handlePost.bind(this);
+        this.handleDeleteOne=this.handleDeleteOne.bind(this);
         this.state={
             posts:'',
-            key:'',
+            key: new Date(),
             msgs: []
         }
     }
+    
+    handleDeleteOne(key){
+        const filter =this.state.msgs.filter(msg=>msg.key!==key);
+        this.setState({
+            msgs:filter
+        })
+    }
     handlePost(e){
         this.setState({
-                posts:e.target.value,
-                key:Date.now() 
+                posts:e.target.value
         })
 
+    }
+
+    handleDeleteAll(){
+        this.setState({
+            msgs:[]
+        })
     }
 
     addPost(e){
@@ -26,32 +42,34 @@ class App extends React.Component{
         console.log(newPost)
         this.setState({msgs :  this.state.msgs.concat(this.state.posts)})
     }
-    
     render(){
+        const subtitle = 'Write something you would like to share'
         return(
             <div>
-                <h1>Postify</h1>
-                <form onSubmit={this.addPost}>
-                    <input 
-                        type="text" 
-                        value={this.state.posts} 
-                        onChange={this.handlePost}
-                    />
-                    <button
-                        type="submit"
-                    >
-                    Add
-                    </button>
+                    <Header subitle = {subtitle}/>
+                    <form onSubmit={this.addPost}>
+                        <input 
+                            type="text" 
+                            value={this.state.posts} 
+                            onChange={this.handlePost}
+                        />
+                        <button type="submit">Add</button>
                 </form>
-                <ul>
-                {this.state.msgs.map((msg)=>
+                {
+                    this.state.msgs.map((msg)=>
                     
-                    <li key={msg}>{msg}</li>)}
-                </ul>
-            </div>      
+                    <div><h1 key={this.state.key}>{msg}</h1><button onClick={this.handleDeleteOne()}>Delete</button> </div>)
+                }
+                <Delete
+                msgs={this.state.msgs} 
+                handleDeleteAll={this.handleDeleteAll}
+                />
+        </div>
+            
         )
     }
 }
+ 
 
 
 
@@ -69,9 +87,28 @@ class App extends React.Component{
 
 
 
+const Header = (props) => {
+    return (
+      <div>
+        <h1>{props.title}</h1>
+        {props.subtitle && <h2>{props.subtitle}</h2>}
+      </div>
+    );
+};
 
+Header.defaultProps={
+    title:'Postify'
+}
 
+    
 
+  const Delete =(props)=>{
+      return(
+          <div>
+            <button onClick ={props.handleDeleteAll}>Delete All</button>
+          </div>
+      )
+  }
 
 
 
